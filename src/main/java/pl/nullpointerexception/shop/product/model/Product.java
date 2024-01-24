c76691bd0b37b3d7da881088bbe19d58ee1fbeee
Krzysztof <krzysztof.b.bielecki@gmail.com>
2024-01-24 10:11:33 +0100

    pobieranie produktów z bazy danych

--- src/main/java/pl/nullpointerexception/shop/product/model/Product.java
+++ src/main/java/pl/nullpointerexception/shop/product/model/Product.java
@@ -1,13 +1,23 @@
 package pl.nullpointerexception.shop.product.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import java.math.BigDecimal;
+import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+@Entity
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private String category;
     private String description;
