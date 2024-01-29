1747edfd0b3a92b3896e6a7d673188eaa862f464
Krzysztof <krzysztof.b.bielecki@gmail.com>
2024-01-29 18:39:43 +0100

    edycja produktów w panelu admina

--- src/main/java/pl/nullpointerexception/shop/admin/product/model/AdminProduct.java
+++ src/main/java/pl/nullpointerexception/shop/admin/product/model/AdminProduct.java
@@ -1,13 +1,19 @@
 package pl.nullpointerexception.shop.admin.product.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Entity
 @Table(name = "product")
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AdminProduct {
 
     @Id
