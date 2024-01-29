6cdabdbe38dc91961f0df9f688bb56bac6858a10
Krzysztof <krzysztof.b.bielecki@gmail.com>
2024-01-29 10:51:46 +0100

    katalog i stronicowanie produktów dla panelu admina

---
+++ src/main/java/pl/nullpointerexception/shop/admin/product/model/AdminProduct.java
@@ -0,0 +1,21 @@
+package pl.nullpointerexception.shop.admin.product.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "product")
+@Getter
+public class AdminProduct {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String category;
+    private String description;
+    private BigDecimal price;
+    private String currency;
+}
